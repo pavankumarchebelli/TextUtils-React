81a721516ceacbb99a89bcd64289d73fb9704c1c
pavankumarchebelli <76152858+pavankumarchebelli@users.noreply.github.com>
2021-08-25 21:29:10 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,13 +29,13 @@ export default function TextForm(props) {
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="3"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert to LowerCase</button>
       </form>
     </div>
     <div className="container my-2" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
     </div>
